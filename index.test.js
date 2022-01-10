2f37f23199b492fa887da8d2ccf9784c29f97e4c
Pradeep A <ps1@duck.com>
2022-01-10 16:51:07 +0530

    Test case for Exception error

--- index.test.js
+++ index.test.js
@@ -9,3 +9,16 @@ test("Get Balance", async () => {
   );
   expect(balance).toBe(0);
 });
+
+test("Get Balance - Exception Error", async () => {
+  const testNode = new newrl.Node("http://3.6.236.206:8090/");
+  try {
+    await testNode.getBalance(
+      "TOKEN_IN_WALLET",
+      "0xc29193dbab0fe018d878e258c93064f01210ec1a",
+      9
+    );
+  } catch ({ message }) {
+    expect(message).toBe("Request failed with status code 404");
+  }
+});
