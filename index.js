2b9150a29ff09b41297800788c7f4c80431c8845
Pradeep A <ps1@duck.com>
2022-01-18 18:35:14 +0530

    Added new methods and class at main level

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const Node = require("./main/Node");
+const Wallet = require("./main/Wallet");
+const getFileHash = require("./main/Hash");
+const signTransaction = require("./main/Signer");
 
 module.exports = {
   Node,
+  Wallet,
+  getFileHash,
+  signTransaction,
 };
